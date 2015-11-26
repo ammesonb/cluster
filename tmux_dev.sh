1198f2987662dac2665b111c45eb189801d3cdc4
Brett Ammeson <ammesonb@gmail.com>
2015-11-25 23:35:24 -0500

    Updates todo grep command in tmux script to not show directories

--- tmux_dev.sh
+++ tmux_dev.sh
@@ -6,4 +6,4 @@ tmux -S /tmp/cluster_dev new-window -t cluster_dev -n host \; send-keys "vi host
 tmux -S /tmp/cluster_dev new-window -t cluster_dev -n service \; send-keys "vi service.cpp" Enter \; split-window -h \; select-layout main-vertical \; send-keys -t 1 "vi service.h" Enter
 tmux -S /tmp/cluster_dev new-window -t cluster_dev -n conf \; send-keys "vi cluster.conf" Enter \; split-window -h \; send-keys -t 1 "vi hosts" Enter \; split-window -v \; send-keys -t 2 "vi services" Enter
 tmux -S /tmp/cluster_dev new-window -t cluster_dev -n make \; split-window -h \; select-layout main-vertical \; send-keys -t 1 "vi Makefile" Enter
-tmux -S /tmp/cluster_dev new-window -t cluster_dev -n todo \; send-keys "grep -iHn todo *" Enter
+tmux -S /tmp/cluster_dev new-window -t cluster_dev -n todo \; send-keys "reset; grep -iHn todo * 2>&1 | grep -i todo" Enter
