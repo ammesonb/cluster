8defe48b7bd369221335745908aa390f3b40da11
Brett Ammeson <ammesonb@gmail.com>
2015-11-28 00:10:29 -0500

    Adds git terminal to tmux script

--- service.cpp
+++ service.cpp
@@ -32,7 +32,6 @@ namespace Cluster {
             }
         }
 
-
     }/*}}}*/
 
     void Service::start() {/*{{{*/
--- tmux_dev.sh
+++ tmux_dev.sh
@@ -7,3 +7,4 @@ tmux -S /tmp/cluster_dev new-window -t cluster_dev -n service \; send-keys "vi s
 tmux -S /tmp/cluster_dev new-window -t cluster_dev -n conf \; send-keys "vi cluster.conf" Enter \; split-window -h \; send-keys -t 1 "vi hosts" Enter \; split-window -v \; send-keys -t 2 "vi services" Enter
 tmux -S /tmp/cluster_dev new-window -t cluster_dev -n make \; split-window -h \; select-layout main-vertical \; send-keys -t 1 "vi Makefile" Enter
 tmux -S /tmp/cluster_dev new-window -t cluster_dev -n todo \; send-keys "reset; grep -iHn todo * 2>&1 | grep -i todo" Enter
+tmux -S /tmp/cluster_dev new-window -t cluster_dev -n git \; send-keys "git pull" Enter
